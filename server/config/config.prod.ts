dd06b5d6687a518c1bd404c8a3919e8f43a4aa6b
shangyongchao <yongchaoshang@outlook.com>
2024-03-22 14:52:05 +0800

    🐞 fix(config): 数据库连接信息

--- server/config/config.prod.ts
+++ server/config/config.prod.ts
@@ -1,10 +1,10 @@
 // 生产环境配置
 export default () => ({
   mysql: {
-    host: '10.0.224.4',
-    port: 25532 || 3306,
+    host: '120.53.225.40',
+    port: 53306,
     username: 'root',
-    password: '',
+    password: '123456',
     database: 'nest_cms_api',
     synchronize: false,
   },
